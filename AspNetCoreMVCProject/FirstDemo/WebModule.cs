523237211035e27743fe7119e466fadf54a866a5
hridoyhr <hridoyswe205@gmail.com>
2021-05-08 17:17:12 +0600

    Dependency Injection Build in Configuration and  Autofac Configuration

---
+++ AspNetCoreMVCProject/FirstDemo/WebModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using FirstDemo.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstDemo
+{
+    public class WebModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SimpleDatabaseService>().As<IDatabaseService>()
+                .InstancePerLifetimeScope();
+
+            
+            base.Load(builder);
+        }
+    }
+}
