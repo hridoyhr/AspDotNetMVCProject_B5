61c21cad80959e195c07b01287a1dc68824668bf
hridoyhr <hridoyswe205@gmail.com>
2021-06-14 23:47:53 +0600

    Ado.net Example

---
+++ AspNetCoreMVCProject/AdoNetExample/AdoNetExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AdoNetExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "Server=DESKTOP-CPJOTGU\\SQLEXPRESS;Database=AspnetCoreB5;User Id=aspnetCoreB5;Password=1234;";
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+
+            using SqlCommand command = new SqlCommand();
+            command.CommandText = "delete student where weight = 45";
+            command.Connection = connection;
+            command.ExecuteNonQuery();
+
+            Console.WriteLine("Successfuly");
+        }
+    }
+}
